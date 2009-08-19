fab4b274daf48ad53fab49e891405ed512cf733f
Tomas Doran (t0m) <t0m@state51.co.uk>
2009-08-19 17:29:37 +0100

    Remove windows line endings and trailing space

--- lib/Catalyst/TraitFor/Request/PerLanguageDomains.pm
+++ lib/Catalyst/TraitFor/Request/PerLanguageDomains.pm
@@ -9,24 +9,24 @@ use namespace::autoclean;
 sub language { 
 	my $self   = shift;
 	my $host   = $self->uri->host;
-	
+
 	if($host   =~ m{^(\w{2})\.})
 	{
 		return $1;
 	}
-	
+
 	my $session = $self->_context->session->{'language'};
 	if($session)
 	{
 		return $session;
 	}
-	
+
 	my $header = $self->headers->header('Accept-language');
 	if($header)
 	{
 		return $header;
 	}
-	
+
 	return undef;
 }
 
