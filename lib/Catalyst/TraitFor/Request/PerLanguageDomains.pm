5699e030d2c30e47a94781daedd96c7833eee3e5
Tomas Doran (t0m) <t0m@state51.co.uk>
2009-08-19 17:32:28 +0100

    Cleanups, add deps

--- lib/Catalyst/TraitFor/Request/PerLanguageDomains.pm
+++ lib/Catalyst/TraitFor/Request/PerLanguageDomains.pm
@@ -1,33 +1,28 @@
 package Catalyst::TraitFor::Request::PerLanguageDomains;
 use Moose::Role;
-use MooseX::Types::Moose qw/ ArrayRef ClassName Object /;
-use MooseX::Types::Common::String qw/ NonEmptySimpleStr /;
 use I18N::AcceptLanguage;
 use namespace::autoclean;
 
+sub language {
+	my $self = shift;
+	my $host = $self->uri->host;
 
-sub language { 
-	my $self   = shift;
-	my $host   = $self->uri->host;
-
-	if($host   =~ m{^(\w{2})\.})
-	{
+	if ($host =~ m{^(\w{2})\.}) {
 		return $1;
 	}
 
 	my $session = $self->_context->session->{'language'};
-	if($session)
-	{
+	if ($session) {
 		return $session;
 	}
 
 	my $header = $self->headers->header('Accept-language');
-	if($header)
-	{
+	if ($header) {
 		return $header;
 	}
 
-	return undef;
+	return;
 }
 
 1;
+
